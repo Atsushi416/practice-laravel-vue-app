4fcdae32988af4850cc16105f59de603736be2df
aseki <atsushi.seki416@gmail.com>
2021-08-09 18:03:48 +0900

    vue install

---
+++ src/app/app/resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <div class="card-header">Practice Laravel Vue App</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
